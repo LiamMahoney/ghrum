97ffcc97d256302d1c150160dd34dcbbbdef5e70
LiamMahoney <liammahoney96@gmail.com>
2022-03-02 21:14:15 -0600

    logging to stdout

--- utils/log.js
+++ utils/log.js
@@ -1,25 +1,14 @@
 const winston = require('winston');
 const path = require('path');
 
-let logFile = process.env.LOG_FILE ? process.env.LOG_FILE : "./ghrum.log";
-let maxLogSize = process.env.MAX_LOG_SIZE ? int(process.env.MAX_LOG_SIZE) : 10000000;
-let maxLogFiles = process.env.MAX_LOG_FILES ? int(process.env.MAX_LOG_FILES) : 5;
-
 const log = winston.createLogger({
     format: winston.format.combine(
         winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
         winston.format.printf(info => `${info.timestamp} [${info.level}] ${info.message}`)
     ),
     transports: [
-        new winston.transports.File({
-            level: "info",
-            filename: logFile,
-            maxsize: maxLogSize,
-            maxFiles: maxLogFiles,
-            tailable: true,
-            zippedArchive: true,
-            json: true,
-            timestamp: true
+        new winston.transports.Console({
+            level: process.env.LOG_LEVEL ? process.env.LOG_LEVEL : 'info'
         })
     ]
 });
